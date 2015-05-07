b0704074d928469b1427fbfe10d43bec9d9910e6
khgl <khayyam.guliyev@epfl.ch>
2015-05-07 18:29:23 +0200

    RDD[Key, Value] join example

---
+++ RDDKeyJoin.scala
@@ -0,0 +1,12 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+class RDDRKeyJoin {
+	def main(args: Array[String]) {
+      val sc = new SparkContext(new SparkConf().setAppName("RDD (Key, Value) join Example"))
+
+      val orders = Utility.getOrdersRDD(sc, "hdfs:///user/guliyev/sf1/orders.tbl").map(o => (o.O_ORDERKEY, o))
+      val lineitem = Utility.getLineItemsRDD(sc,"hdfs:///user/guliyev/sf1/lineitem.tbl").map(l => (l.L_ORDERKEY, l))
+
+      orders.join(lineitem).collect()
+  }
+}
